642877b72813d1c275a465b9818b365f803695cc
jamal7X7 <jamal7x7@gmail.com>
2018-10-05 21:53:23 +0000

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react'
+import '../styles/App.css'
+
+const Header = () => (
+  <header className='App-header'>
+    <h1 className='App-title'> Balance App </h1>
+  </header>
+)
+
+const Main = (props) => (
+  <div className='game'>
+
+    
+
+  </div>
+)
+
+const Side = () => (
+  <div className='score'>
+
+      
+
+  </div>
+)
+
+
+
+class BalanceApp extends Component {
+  
+  state = {
+    
+  }
+
+
+
+  componentDidMount() {
+    
+  
+
+  }
+
+
+      render () {
+    return (
+      <div className='App-container'>
+
+        <Side />
+        <Main />
+       
+
+      </div>
+    )
+  }
+}
+
+const App = (props) => (
+  <div className='App'>
+    <Header />
+    <BalanceApp />
+
+  </div>
+)
+
+export default App
+
